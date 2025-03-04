9878baef38ce13bd9b13f598d9d635c53dd7f4c7
Nick Veltmaat <80262318+nickveltmaat@users.noreply.github.com>
2025-03-04 14:09:49 +0100

    Update isec_to_vcf.py
    TRUE or FALSE argument added to determine whether or not you want the INFO column parsed

--- isec_to_vcf.py
+++ isec_to_vcf.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python
 
+#!/groups/umcg-pmb/tmp01/projects/hematopathology/Lymphoma/Nick/SNVcallingPipeline/env/bin/python3
+
 """
 This module reads a 'bcftools isec' output file of merged variants, and then converts to standard .vcf format.
 
@@ -9,10 +11,10 @@ Date: 10-2-2023
 
 import sys
 
-if len(sys.argv) != 3:
-  print("Usage:\t" + sys.argv[0] + "\t<path/to/input_file>\t<path/to/output_filename>")
+if len(sys.argv) != 4: #Changed
+  print("Usage:\t" + sys.argv[0] + "\t<path/to/input_file>\t<path/to/output_filename>" + "\t<path/to/input_file>\t<path/to/output_filename>" + 'Parse Info (TRUE/FALSE)') #Changed
   exit(0)
-  
+
 outfile = open(sys.argv[2], "w")
 outfile.write("##fileformat=VCFv4.3\n")
 
@@ -37,4 +39,11 @@ with open(sys.argv[1]) as infile:
     position = tokens[1]
     ref = tokens[2]
     alt = tokens[3]
-    outfile.write(chromosome + "\t" + str(position) + "\t" + "." + "\t" + ref.upper() + "\t" + alt.upper() + "\t" + "." + "\t" + "PASS"  + "\t" + ".\n")
+    #outfile.write(chromosome + "\t" + str(position) + "\t" + "." + "\t" + ref.upper() + "\t" + alt.upper() + "\t" + "." + "\t" + "PASS"  + "\t" + ".\n")
+    if sys.argv[3].lower() == 'true':
+      info = tokens[4] #changed
+      outfile.write(chromosome + "\t" + str(position) + "\t" + "." + "\t" + ref.upper() + "\t" + alt.upper() + "\t" + "." + "\t" + "PASS" + "\t" + info + "\n")
+    elif sys.argv[3].lower() == 'false':
+      outfile.write(chromosome + "\t" + str(position) + "\t" + "." + "\t" + ref.upper() + "\t" + alt.upper() + "\t" + "." + "\t" + "PASS" + "\t" + ".\n")
+    else:
+      print('Parse TRUE or FALSE to determine whether you want the INFO column kept or not')
