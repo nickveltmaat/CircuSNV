1bc91c15d9f021ec864ab94293ffc2e5b5c27b45
Nick Veltmaat <80262318+nickveltmaat@users.noreply.github.com>
2025-03-04 14:17:40 +0100

    Update sinvict_to_vcf2.py
    added Strand Bias information to output

--- sinvict_to_vcf2.py
+++ sinvict_to_vcf2.py
@@ -10,10 +10,13 @@ Date: 6-2-2023
 import os
 import sys
 import subprocess
-
 if len(sys.argv) != 4:
   print("Usage:\t" + sys.argv[0] + "\t<input_file>\t<output_filename>\t<reference_fasta>")
   exit(0)
+
+#for i in range(len(sys.argv)):
+#  print('Sys.argv ', i , ' is ', sys.argv[i])
+   
   
 outfile = open(sys.argv[2], "w")
 outfile.write("##fileformat=VCFv4.3\n")
@@ -29,11 +32,14 @@ with open(sys.argv[1]) as infile:
 outfile.write('''##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Read Depth">\n''')
 outfile.write('''##FORMAT=<ID=VAF,Number=1,Type=Float,Description="Variant Allele Frequency">\n''')
 outfile.write('''##FORMAT=<ID=MDP,Number=1,Type=Integer,Description="Number of reads supporting the mutation">\n''')
+outfile.write('''##FORMAT=<ID=MSB,Number=2,Type=Integer,Description="Mutant Strand bias: reads supporting the mutation on the plus, minus strand ">\n''')
 
-chroms2 = set(chroms)
-    
-for i in chroms2:
-    outfile.write("##contig=<ID="+str(i)+">\n")
+
+#chroms2 = set(chroms)
+for i in range(1,23):
+    outfile.write("##contig=<ID=chr"+str(i)+">\n")
+outfile.write("##contig=<ID=chrX>\n")
+outfile.write("##contig=<ID=chrY>\n")
 
 outfile.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t"+os.path.basename(sys.argv[1])+"\n")
 
@@ -48,10 +54,13 @@ with open(sys.argv[1]) as infile:
     vaf = round(float(tokens[7])/100, 3)
     depth = int(tokens[4])
     mutdp = int(tokens[6])
+    SBplus = tokens[8].split('+:',2)[1]
+    SBminus = tokens[9].split('-:',2)[1]
+    ######## ADD SB
     if alt[0] == "+" :
       #insertion
       alt = ref + alt[1:]
-      outfile.write(chromosome + "\t" + str(position) + "\t" + "." + "\t" + ref.upper() + "\t" + alt.upper() + "\t" + "." + "\t" + "PASS" + "\t" + "." + "\t" + "DP:VAF:MDP" + "\t" + str(depth)+':'+str(vaf)+':'+str(mutdp)  + "\n")
+      outfile.write(chromosome + "\t" + str(position) + "\t" + "." + "\t" + ref.upper() + "\t" + alt.upper() + "\t" + "." + "\t" + "PASS" + "\t" + "." + "\t" + "DP:VAF:MDP:MSB" + "\t" + str(depth)+':'+str(vaf)+':'+str(mutdp)+':'+str(SBplus) +','+str(SBminus) + "\n")
     elif alt[0] == "-" :
       #deletion
       position = int(position)
@@ -61,8 +70,9 @@ with open(sys.argv[1]) as infile:
       base.rstrip()
       ref = base + str(alt[1:]).encode()
       alt = base
-      outfile.write(chromosome + "\t" + str(position) + "\t" + "." + "\t" + ref.decode("utf-8").upper() + "\t" + alt.decode("utf-8").upper() + "\t" + "." + "\t" + "PASS"  + "\t" + "." + "\t" + "DP:VAF:MDP" + "\t" + str(depth)+':'+str(vaf)+':'+str(mutdp) + "\n")
+      outfile.write(chromosome + "\t" + str(position) + "\t" + "." + "\t" + ref.decode("utf-8").upper() + "\t" + alt.decode("utf-8").upper() + "\t" + "." + "\t" + "PASS"  + "\t" + "." + "\t" + "DP:VAF:MDP:MSB" + "\t" + str(depth)+':'+str(vaf)+':'+str(mutdp)+':'+str(SBplus) +','+str(SBminus) + "\n")
     else: 
-      outfile.write(chromosome + "\t" + str(position) + "\t" + "." + "\t" + ref.upper() + "\t" + alt.upper() + "\t" + "." + "\t" + "PASS"  + "\t" + "." + "\t" + "DP:VAF:MDP" + "\t" + str(depth)+':'+str(vaf)+':'+str(mutdp) + "\n")
+      outfile.write(chromosome + "\t" + str(position) + "\t" + "." + "\t" + ref.upper() + "\t" + alt.upper() + "\t" + "." + "\t" + "PASS"  + "\t" + "." + "\t" + "DP:VAF:MDP:MSB" + "\t" + str(depth)+':'+str(vaf)+':'+str(mutdp)+':'+str(SBplus) +','+str(SBminus) + "\n")
 
+      
 # original copied and modified at 6 feb 2023 from: https://github.com/sfu-compbio/sinvict/blob/master/sinvict_to_vcf.py
