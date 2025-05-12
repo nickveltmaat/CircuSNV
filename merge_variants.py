49488fa86060f6e1fd5e57ccad3a17c5a4f013dc
Nick Veltmaat <80262318+nickveltmaat@users.noreply.github.com>
2025-05-12 15:26:05 +0200

    Update merge_variants.py
    Updated argument parsing, string split syntax and more

--- merge_variants.py
+++ merge_variants.py
@@ -1,5 +1,3 @@
-#! /usr/bin/python
-
 """
 This module reads a 'bcftools isec' output file of merged variants (sites.txt) and adds the data from the raw VCF files to it (RD, VAF & MRD)
 
@@ -11,16 +9,26 @@ import pandas as pd
 import io
 import sys
 import gzip
+import argparse
 
-if len(sys.argv) != 3:
-  print("Usage:\t" + sys.argv[0] + "\t<samplename>\t<FilterMode>")
-  exit(0)
+parser = argparse.ArgumentParser(description="Merge VCF variant data with bcftools isec output.")
 
-samplename = str(sys.argv[1])
-filtermode = str(sys.argv[2])
-path = './output/'
+parser.add_argument("samplename", type=str, help="Name of the sample.")
+parser.add_argument("filtermode", type=str, help="Filtering mode: 'phased', 'phased_pon', etc.")
+parser.add_argument("--infile", type=str, default=None, help="Input file (optional, depending on mode).")
+parser.add_argument("--outfile", type=str, default=None, help="Output file (optional, depending on mode).")
+parser.add_argument("outdir", type=str, help="Output directory.")
 
+args = parser.parse_args()
 
+samplename = args.samplename
+filtermode = args.filtermode
+infile = args.infile
+outfile = args.outfile
+outdir = args.outdir
+if not outdir.endswith('/'):
+    outdir += '/'
+    
 #General Functions:
 #####
 def read_vcf(path):
@@ -50,66 +58,71 @@ def vcf_info_vardict(df):
     '''
     This functions loads a Vardict .vcf df and extracs 'RD', 'VAF' & 'MRD' data into new columns
     '''
-    df['MRD'] = df[df.columns[9]].str.split(':',5).str[2]
-    df['AF'] = df[df.columns[9]].str.split(':',5).str[4]
-    df['RD'] = df[df.columns[9]].str.split(':',5).str[1]
+    df['MRD'] = df[df.columns[9]].str.split(':').str[2]
+    df['AF'] = df[df.columns[9]].str.split(':').str[4]
+    df['RD'] = df[df.columns[9]].str.split(':').str[1]
     return df
 
 def vcf_info_lofreq(df):
     '''
     This functions loads a Lofreq .vcf df and extracs 'RD', 'VAF' & 'MRD' data into new columns
     '''
-    df['AF'] = df['INFO'].str.split('AF=',1).str[1].str.split(';SB',1).str[0].astype(float)
-    df['RD'] = df['INFO'].str.split('DP=',1).str[1].str.split(';AF',1).str[0].astype(int)
-    df['MRD'] = df['INFO'].str.split('DP4=',1).str[1].str.split(';',1).str[0].str.split(',',4).str[2:].apply(lambda x: sum([int(i) for i in x])).astype(int)
+    df['AF'] = df['INFO'].str.split('AF=').str[1].str.split(';SB').str[0].astype(float)
+    df['RD'] = df['INFO'].str.split('DP=').str[1].str.split(';AF').str[0].astype(int)
+    df['MRD'] = df['INFO'].str.split('DP4=').str[1].str.split(';').str[0].str.split(',').str[2:].apply(lambda x: sum([int(i) for i in x])).astype(int)
     return df
 
 def vcf_info_mutect(df):
     '''
     This functions loads a Mutect2 .vcf df and extracs 'RD', 'VAF' & 'MRD' data into new columns
     '''
-    df['AF'] = df[df.columns[9]].str.split(':', 5).str[2].astype(float)
-    df['RD'] = df[df.columns[9]].str.split(':', 5).str[3].astype(int)
-    df['MRD'] = df[df.columns[9]].str.split(':', 5).str[1].str.split(',').str[1]
+    df['AF'] = df[df.columns[9]].str.split(':').str[2].astype(float)
+    df['RD'] = df[df.columns[9]].str.split(':').str[3].astype(int)
+    df['MRD'] = df[df.columns[9]].str.split(':').str[1].str.split(',').str[1]
     return df
 
 def vcf_info_sinvict(df):
     '''
     This functions loads a Sinvict .vcf df and extracs 'RD', 'VAF' & 'MRD' data into new columns
     '''
-    df['MRD'] = df['calls_level1.sinvict'].str.split(':',4).str[2].astype(int)
-    df['RD'] = df['calls_level1.sinvict'].str.split(':',4).str[0].astype(int)
-    df['AF'] = df['calls_level1.sinvict'].str.split(':',4).str[1].astype(float)
+    #df['MRD'] = df['calls_level1.sinvict'].str.split(':',4).str[2].astype(int)
+    df['MRD'] = df['calls_level1.sinvict'].str.split(':').str[2].astype(int)
+    df['RD'] = df['calls_level1.sinvict'].str.split(':').str[0].astype(int)
+    df['AF'] = df['calls_level1.sinvict'].str.split(':').str[1].astype(float)
     return df
 ########
 
 
 # Reading vcf files:
-if filtermode == 'PerTool':
-    sites = read_sites(path + samplename + '/PerTool_PoN-filtered/sites.txt') #Reads PerTool-PoN-filtered sites
-    dfsv = read_vcf(path + samplename + '/SiNVICT_PoN-filtered/0000.vcf.gz') #reads .vcf files of merged vcfs to retreive RD, VAF & MRD info
-    dfm2 = read_vcf(path + samplename + '/Mutect2_PoN-filtered/0000.vcf.gz')
-    dflf = read_vcf(path + samplename + '/LoFreq_PoN-filtered/0000.vcf.gz')
-    dfvd = read_vcf(path + samplename + '/VarDict_PoN-filtered/0000.vcf.gz')
-elif filtermode == 'Combined':
-    sites = read_sites(path + samplename + '/Combined_PoN-filtered/merged_PoN-filtered/sites.txt') #Reads Combined-PoN-filtered sites
-    sites2 = read_sites(path + samplename + '/Combined_PoN-filtered/sites.txt') #Reads unfiltered sites, to see which tools called the mutation. 
+if filtermode == 'no_pon':
+    sites = read_sites(outdir + samplename + '/isec/sites.txt') #Reads PerTool-PoN-filtered sites
+    dfsv = read_vcf(outdir + samplename + '/isec/0000.vcf.gz') #reads .vcf files of merged vcfs to retreive RD, VAF & MRD info
+    dfm2 = read_vcf(outdir + samplename + '/isec/0001.vcf.gz')
+    dflf = read_vcf(outdir + samplename + '/isec/0002.vcf.gz')
+    dfvd = read_vcf(outdir + samplename + '/isec/0003.vcf.gz')
+elif filtermode == 'pon':
+    sites = read_sites(outdir + samplename + '/isec/PoN-filtered/sites.txt') #Reads Combined-PoN-filtered sites
+    sites2 = read_sites(outdir + samplename + '/isec/sites.txt') #Reads unfiltered sites, to see which tools called the mutation. 
     sites['TOOLS'] = pd.merge(sites, sites2, on='loc')['TOOLS_y']  #replaces 'TOOLS column'
-    dfsv = read_vcf(path + samplename + '/Combined_PoN-filtered/0000.vcf.gz') #reads .vcf files of merged vcfs to retreive RD, VAF & MRD info
-    dfm2 = read_vcf(path + samplename + '/Combined_PoN-filtered/0001.vcf.gz')
-    dflf = read_vcf(path + samplename + '/Combined_PoN-filtered/0002.vcf.gz')
-    dfvd = read_vcf(path + samplename + '/Combined_PoN-filtered/0003.vcf.gz')
+    dfsv = read_vcf(outdir + samplename + '/isec/0000.vcf.gz') #reads .vcf files of merged vcfs to retreive RD, VAF & MRD info
+    dfm2 = read_vcf(outdir + samplename + '/isec/0001.vcf.gz')
+    dflf = read_vcf(outdir + samplename + '/isec/0002.vcf.gz')
+    dfvd = read_vcf(outdir + samplename + '/isec/0003.vcf.gz')
+elif filtermode == 'phased_pon':
+    sites = read_sites(outdir + samplename + '/mnvcalling/ponfiltered/sites.txt') #Reads unfiltered sites.
+    sites2 = read_sites(outdir + samplename + '/mnvcalling/ponfiltered2/sites.txt') #Reads unfiltered sites. 
+    dfsv = read_vcf(outdir + samplename + '/mnvcalling/SiNVICT_MNV.vcf.gz') #reads .vcf files of merged vcfs to retreive RD, VAF & MRD info
+    dfm2 = read_vcf(outdir + samplename + '/mnvcalling/Mutect2_MNV.vcf.gz')
+    dflf = read_vcf(outdir + samplename + '/mnvcalling/LoFreq_MNV.vcf.gz')
+    dfvd = read_vcf(outdir + samplename + '/mnvcalling/VarDict_MNV.vcf.gz')
 elif filtermode == 'phased':
-    sites = read_sites(path + samplename + '/Combined_PoN-filtered/sites.txt') #Reads unfiltered sites. 
-    dfsv = read_vcf(path + samplename + '/Combined_PoN-filtered/0000.vcf.gz') #reads .vcf files of merged vcfs to retreive RD, VAF & MRD info
-    dfm2 = read_vcf(path + samplename + '/Combined_PoN-filtered/0001.vcf.gz')
-    dflf = read_vcf(path + samplename + '/Combined_PoN-filtered/0002.vcf.gz')
-    dfvd = read_vcf(path + samplename + '/Combined_PoN-filtered/0003.vcf.gz')
-    
+    sites = read_sites(outdir + samplename + '/mnvcalling/' + infile) #Reads unfiltered sites. 
+    dfsv = read_vcf(outdir + samplename + '/mnvcalling/SiNVICT_MNV.vcf.gz') #reads .vcf files of merged vcfs to retreive RD, VAF & MRD info
+    dfm2 = read_vcf(outdir + samplename + '/mnvcalling/Mutect2_MNV.vcf.gz')
+    dflf = read_vcf(outdir + samplename + '/mnvcalling/LoFreq_MNV.vcf.gz')
+    dfvd = read_vcf(outdir + samplename + '/mnvcalling/VarDict_MNV.vcf.gz')
     
 #Elif filtermode = non existant...
-    
-    
     
 #Extracting RD, VAF & MRD data per tool.vcf
 dfsv = vcf_info_sinvict(dfsv).rename(columns = {'AF':'AFsv', 'RD':'RDsv', 'MRD':'MRDsv'})
@@ -134,19 +147,52 @@ df_merged = pd.merge(pd.merge(pd.merge(pd.merge(dfm2, dflf, on='loc', how='outer
                                        dfsv, on='loc', how='outer'), 
                               dfvd, on='loc', how='outer'), 
                      sites, on='loc', how='inner')
-df_merged["AF_mean"] = df_merged[["AFm2", "AFlf", "AFsv", "AFvd"]].apply(pd.to_numeric, args=['coerce']).mean(axis=1, skipna = True).round(4)
-df_merged["DP_mean"] = df_merged[["RDm2", "RDlf", "RDsv", "RDvd"]].apply(pd.to_numeric, args=['coerce']).mean(axis=1, skipna = True).round(0).astype(int)
-df_merged["MRD_mean"] = df_merged[["MRDm2", "MRDlf", "MRDsv", "MRDvd"]].apply(pd.to_numeric, args=['coerce']).mean(axis=1, skipna = True).round(0).astype(int)
-df_merged['info'] = 'SV-M2-LF-VD:' + df_merged['TOOLS'] + '_AF:' + df_merged['AF_mean'].astype(str) + '_DP:' + df_merged['DP_mean'].astype(str)+ '_MRD:' + df_merged['MRD_mean'].astype(str)
-df_merged = df_merged[['CHROM', 'POS','REF','ALT','info']] 
 
-#Sort on position (chrom, pos)
-df_merged['chrnum'] = df_merged['CHROM'].str.extract(r'(\d+|[XY])').replace({'X': 23, 'Y': 24}).astype(int)
-df_merged = df_merged.sort_values(by=['chrnum', 'POS']).drop(columns =['chrnum'])
 
-if filtermode == 'PerTool':
-  df_merged.to_csv(path + samplename + '/merged_vcfs.txt', sep='\t', index = False, header= False)
-elif filtermode == 'Combined':
-  df_merged.to_csv(path + samplename + '/merged_vcfs.txt', sep='\t', index = False, header= False)
+
+
+
+
+
+def process_dataframe(df_merged):
+  df_merged["AF_mean"] = df_merged[["AFm2", "AFlf", "AFsv", "AFvd"]].apply(pd.to_numeric, args=['coerce']).mean(axis=1, skipna = True).round(4)
+  df_merged["DP_mean"] = df_merged[["RDm2", "RDlf", "RDsv", "RDvd"]].apply(pd.to_numeric, args=['coerce']).mean(axis=1, skipna = True).round(0).astype(int)
+  df_merged["MRD_mean"] = df_merged[["MRDm2", "MRDlf", "MRDsv", "MRDvd"]].apply(pd.to_numeric, args=['coerce']).mean(axis=1, skipna = True).round(0).astype(int)
+  df_merged['info'] = 'SV-M2-LF-VD:' + df_merged['TOOLS'] + '_AF:' + df_merged['AF_mean'].astype(str) + '_DP:' + df_merged['DP_mean'].astype(str)+ '_MRD:' + df_merged['MRD_mean'].astype(str)
+  df_merged = df_merged[['CHROM', 'POS','REF','ALT','info']].copy()
+  
+  #Sort on position (chrom, pos)
+  df_merged['chrnum'] = df_merged['CHROM'].str.extract(r'(\d+|[XY])').replace({'X': 23, 'Y': 24}).astype(int)
+  df_merged = df_merged.sort_values(by=['chrnum', 'POS']).drop(columns =['chrnum'])
+    
+  return df_merged
+
+    
+    
+# Example usage:
+df_merged = process_dataframe(df_merged).copy()
+
+
+
+
+if filtermode == 'phased_pon':
+  df_merged2 = pd.merge(pd.merge(pd.merge(pd.merge(dfm2, dflf, on='loc', how='outer'), 
+                                         dfsv, on='loc', how='outer'), 
+                                dfvd, on='loc', how='outer'), 
+                       sites2, on='loc', how='inner')
+  df_merged2 = process_dataframe(df_merged2).copy()
+else:
+  pass
+
+
+if filtermode == 'no_pon':
+  df_merged.to_csv(outdir + samplename + '/merged_vcfs.txt', sep='\t', index = False, header= False)
+elif filtermode == 'pon':
+  df_merged.to_csv(outdir + samplename + '/merged_vcfs.txt', sep='\t', index = False, header= False)
 elif filtermode == 'phased':
-  df_merged.to_csv(path + samplename + '/merged_vcfs_phased.txt', sep='\t', index = False, header= False)
+  df_merged.to_csv(outdir + samplename + '/mnvcalling/' + outfile, sep='\t', index = False, header= False)
+elif filtermode == 'phased_pon':
+  df_merged.to_csv(outdir + samplename + '/mnvcalling/merged_vcfs_phased_mnv.txt', sep='\t', index = False, header= False) #SNP filtered --> pon filtered
+  df_merged2.to_csv(outdir + samplename + '/mnvcalling/sites_pon_filtered.txt', sep='\t', index = False, header= False) #Is extra: Only PoN filtered
+
+ 
